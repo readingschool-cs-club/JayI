38cde176f45c0fbe166582e7dae4cb3418c0a46f
Saleem Rashid <saleemrashid1@hotmail.com>
2015-03-24 18:05:09 +0000

    Bot: warning on execution

--- Bot.py
+++ Bot.py
@@ -2,3 +2,15 @@ from JayI import JayI
 
 class Bot:
     pass
+
+if __name__ == "__main__":
+    print(r"""
+
+  +-------------------------+
+  |  DO NOT RUN THIS FILE!  |
+  +-------------------------+
+  
+  This is an abstract class for
+  use in a specific implementation!
+
+    """)
