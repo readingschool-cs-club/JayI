0fbc77900208da32a0600bc00a95b238c968b964
Saleem Rashid <saleemrashid1@hotmail.com>
2015-03-25 06:38:00 +0000

    Gitter: initial transition to multi room

--- Gitter.py
+++ Gitter.py
@@ -5,31 +5,34 @@ import json
 import re
 
 class GitterBot(Bot):
-    def __init__(self, token, room):
+    def __init__(self, token):
         super().__init__()
         self.token = token
-        self.room = room
         self.headers = {
             "Authorization": "Bearer %s" % self.token,
         }
-        
+
         self.userId = self.get_userId()
-        self.connect()
+        self.connect(self.get_rooms()[0])
+
+    def get_rooms(self):
+        r = requests.get("https://api.gitter.im/v1/rooms", headers=self.headers)
+        return [room["id"] for room in r.json()]
 
     def get_userId(self):
         r = requests.get("https://api.gitter.im/v1/user", headers=self.headers)
         return r.json()[0]["id"]
 
-    def connect(self):
-        r = requests.get("https://stream.gitter.im/v1/rooms/%s/chatMessages" % self.room,
+    def connect(self, room):
+        r = requests.get("https://stream.gitter.im/v1/rooms/%s/chatMessages" % room,
                 headers=self.headers, stream=True)
 
         for line in r.iter_lines(chunk_size=1):
             line = line.decode("utf-8")
             if line.strip():
-                self.load(json.loads(line))
+                self.load(json.loads(line), room)
 
-    def load(self, data):
+    def load(self, data, room):
         mention = None
         for user in data["mentions"]:
             if "userId" in user and user["userId"] == self.userId:
@@ -42,13 +45,13 @@ class GitterBot(Bot):
         mention = "@" + mention
         trigger = re.sub(r"\s*%s\s*" % re.escape(mention), " ", data["text"]).strip()
 
-        self.write(data["fromUser"], trigger)
+        self.write(data["fromUser"], trigger, room)
 
-    def write(self, user, trigger):
+    def write(self, user, trigger, room):
         response = self.respond(user["id"], trigger)
         if response:
-            self.send("@" + user["username"] + " " + response)
+            self.send("@" + user["username"] + " " + response, room)
 
-    def send(self, text):
-        requests.post("https://api.gitter.im/v1/rooms/%s/chatMessages" % self.room,
+    def send(self, text, room):
+        requests.post("https://api.gitter.im/v1/rooms/%s/chatMessages" % room,
             headers=self.headers, data={"text":text})
